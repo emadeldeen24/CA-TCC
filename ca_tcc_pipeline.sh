9473674d1a67ff64d8d48b6d61a8c067430a011f
Emadeldeen Eldele <37911596+emadeldeen24@users.noreply.github.com>
2023-10-13 16:26:23 +0800

    Update ca_tcc_pipeline.sh

--- ca_tcc_pipeline.sh
+++ ca_tcc_pipeline.sh
@@ -10,7 +10,7 @@ do
     python3 main.py --experiment_description $exp --run_description $run --seed $i --selected_dataset $dataset --training_mode "self_supervised"
     python3 main.py --experiment_description $exp --run_description $run --seed $i --selected_dataset $dataset --training_mode "train_linear_1p"
     python3 main.py --experiment_description $exp --run_description $run --seed $i --selected_dataset $dataset --training_mode "ft_1p"
-    python3 main.py --experiment_description $exp --run_description $run --seed $i --selected_dataset $dataset --training_mode "gen_psuedo_labels"
+    python3 main.py --experiment_description $exp --run_description $run --seed $i --selected_dataset $dataset --training_mode "gen_pseudo_labels"
     python3 main.py --experiment_description $exp --run_description $run --seed $i --selected_dataset $dataset --training_mode "SupCon"
     python3 main.py --experiment_description $exp --run_description $run --seed $i --selected_dataset $dataset --training_mode "train_linear_SupCon_1p"
 done
